289cf5abf0988fb82c062b45fa09210a346ef891
Tácio Nery <tacionery@gmail.com>
2016-10-01 12:35:54 -0300

    the user model

---
+++ test/models/user_test.exs
@@ -0,0 +1,18 @@
+defmodule CascalhoScrumBoard.UserTest do
+  use CascalhoScrumBoard.ModelCase
+
+  alias CascalhoScrumBoard.User
+
+  @valid_attrs %{avatar_url: "some content", bio: "some content", email: "some content", encrypted_password: "some content", first_name: "some content", last_name: "some content"}
+  @invalid_attrs %{}
+
+  test "changeset with valid attributes" do
+    changeset = User.changeset(%User{}, @valid_attrs)
+    assert changeset.valid?
+  end
+
+  test "changeset with invalid attributes" do
+    changeset = User.changeset(%User{}, @invalid_attrs)
+    refute changeset.valid?
+  end
+end
