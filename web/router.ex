c0baf930ab64da4c5adec2041278bb77a7e68e23
Luiz Paulo Godoy <luizpaulogodoy@gmail.com>
2016-10-05 17:43:47 -0300

    registration routes

--- web/router.ex
+++ web/router.ex
@@ -17,6 +17,8 @@ defmodule CascalhoScrumBoard.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
+
+    resources "registrations", RegistrationController, only: [:newm, :create]
   end
 
   # Other scopes may use custom stacks.
