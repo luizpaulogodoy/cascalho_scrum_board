de964ccfd1fec74898220e571cbba6dba0cea810
Luiz Paulo Godoy <luizpaulogodoy@gmail.com>
2016-10-05 19:44:27 -0300

    create method in registration controller

--- web/controllers/registration_controller.ex
+++ web/controllers/registration_controller.ex
@@ -6,4 +6,15 @@ defmodule CascalhoScrumBoard.RegistrationController do
     changeset = User.changeset(%User{})
     render conn, changeset: changeset
   end
+
+  def create(conn, %{"user" => user_params}) do
+    changeset = User.changeset(%User{}, user_params)
+
+    case CascalhoScrumBoard.Registration.create(changeset, CascalhoScrumBoard.Repo) do
+      {:ok, changeset} ->
+        # sign in the user
+      {:error, changeset} ->
+        # show error message
+    end
+  end
 end
