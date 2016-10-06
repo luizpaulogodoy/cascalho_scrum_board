1f7e898fc87124e95f06547fc476e002129190c5
Luiz Paulo Godoy <luizpaulogodoy@gmail.com>
2016-10-05 21:09:48 -0300

    add create method on session controller

--- web/controllers/session_controller.ex
+++ web/controllers/session_controller.ex
@@ -4,4 +4,18 @@ defmodule CascalhoScrumBoard.SessionController do
   def new(conn, _params) do
     render conn, "new.html"
   end
+
+  def create(conn, %{"session" => session_params}) do
+    case CascalhoScrumBoard.Session.login(session_params, Blog.repo) do
+      {:ok, user} ->
+        conn
+        |> put_session(:current_user, user.id)
+        |> put_flash(:info, "Logged in")
+        |> redirect(to: "/")
+      :error ->
+        conn
+        |> put_flash(:info, "Wrong email or password")
+        |> render("new.html")
+    end
+  end
 end
